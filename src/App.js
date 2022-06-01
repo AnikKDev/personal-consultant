d90bf77f8a45c0d4e7748affdfae54e4de548aa8
AnikKDev <anikdev2016@gmail.com>
2022-06-01 21:22:23 +0600

    completed banner section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
-
+import About from './components/About/About';
+import Dashboard from './components/Dashboard/Dashboard';
+import Contact from './components/Contact/Contact';
 function App() {
   return (
     <div >
@@ -10,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
       </Routes>
     </div>
   );
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,26 +7,46 @@ import bgimage from '../../assets/images/bg-1.jpg'
 const Banner = () => {
 
     const settings = {
-        dots: true,
+        dots: false,
+        fade: true,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 5000,
         infinite: true,
-        speed: 500,
+        speed: 1800,
         slidesToShow: 1,
         slidesToScroll: 1
     };
     return (
         <div>
             <Slider {...settings}>
-                <div className="banner-container">
-                    <h2 className="text-6xl">hello</h2>
-                    {/* <img src={bgimage} alt="" /> */}
+                <div className="banner-container banner-container1 ">
+                    <div className="flex justify-center items-center min-h-screen">
+                        <div className="text-center">
+                            <h3 className="lg:text-4xl text-2xl uppercase font-bold text-white">We are here to</h3>
+                            <h1 className="uppercase font-bold text-white lg:text-6xl text-3xl mt-6"><span className="text-secondary">Consult</span> your matter</h1>
+                            <button className="uppercase text-white bg-secondary hover:bg-white border-solid  border-0 py-3 my-10 duration-500 transition-all px-7 hover:text-black">Reach Us</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="banner-container">
-                    {/* <img src={bgimage} alt="" /> */}
+                <div className="banner-container banner-container2 ">
+                    <div className="flex justify-center items-center min-h-screen">
+                        <div className="text-center">
+                            <h3 className="lg:text-4xl text-2xl uppercase font-bold text-white">Good care means</h3>
+                            <h1 className="uppercase font-bold text-white lg:text-6xl text-3xl mt-6">personal <span className="text-secondary">Consultantcy</span></h1>
+                            <button className="uppercase text-white bg-secondary hover:bg-white border-solid  border-0 py-3 my-10 duration-500 transition-all px-7 hover:text-black">Reach Us</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="banner-container">
-                    {/* <img src={bgimage} alt="" /> */}
+                <div className="banner-container banner-container3 ">
+                    <div className="flex justify-center items-center min-h-screen">
+                        <div className="text-center">
+                            <h3 className="lg:text-4xl text-2xl uppercase font-bold text-white">We do</h3>
+                            <h1 className="uppercase font-bold text-white lg:text-6xl text-3xl mt-6">What we do <span className="text-secondary">Best</span></h1>
+                            <button className="uppercase text-white bg-secondary hover:bg-white border-solid  border-0 py-3 my-10 duration-500 transition-all px-7 hover:text-black">Reach Us</button>
+                        </div>
+                    </div>
                 </div>
-
             </Slider>
         </div>
     );
